7e466ea6e7943127555c8d60522e19c0bc4ab47a
Kaleb Rigg <kaleb2323rigg@gmail.com>
2021-10-09 09:13:57 -0600

    button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,7 +5,7 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var btn2 = document.getElementsById("but2");
+//var btn2 = document.getElementsById("but2");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -13,8 +13,12 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-btn2.onclick = function() {
-  modal.style.display = "none";
+//btn2.onclick = function() {
+//  modal.style.display = "none";
+//}
+
+function closeForm() {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
