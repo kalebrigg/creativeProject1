143f5a8e7e578bbb026f438b47771aa6333b5b56
Kaleb Rigg <kaleb2323rigg@gmail.com>
2021-10-09 00:24:31 -0600

    fadsf

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,25 @@
+// Get the modal
+var modal = document.getElementById("myModal");
 
-let map;
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-function initMap() {
-    console.log("init map called from callback");
-    map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+//span.onclick = function() {
+//  modal.style.display = "none";
+//}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
